6fa592739ef22a9575c666430cc8e15938be8f3d
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-08-09 20:39:39 +0300

    create article class in models file

--- app/models.py
+++ app/models.py
@@ -10,3 +10,15 @@ class News:
         self.country = country
         self.category = category
         self.id = id
+
+class Articles:
+    '''
+    articles class to define article objects
+    '''
+    def __init__(self, title, description, image, publishedAt, author, url):
+        self.title = title
+        self.description = description
+        self.image = image
+        self.publishedAt = publishedAt
+        self.author = author
+        self.url = url
