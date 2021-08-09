09bdf79c1fe5ec89fc99f361d1812f73534696ab
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-08-09 15:30:31 +0300

    create article path

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
-from flask import render_template
-from flask import current_app
-
+from flask import render_template, request, redirect, url_for
+from . import main
+from ..request import get_articles, get_news
 
 # views
 @main.route('/')
@@ -19,3 +19,24 @@ def index():
 
     return render_template('index.html', title=title, sports=all_news, general=general_news, technology=tech_news,
                            business=bus_news, entertainment=ent_news)
+
+# Views
+@main.route('/news/<int:id>')
+def news(id):
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    news = get_news(id)
+
+    return render_template('index.html', news=news)
+
+
+@main.route('/articles/<source_id>')
+def articles(source_id):
+    '''
+    function that returns articles by source id
+    '''
+
+    article_source = get_articles(source_id)
+    title = f'{source_id}| Articles'
+    return render_template('articles.html', title=title, name=source_id, news=article_source)
