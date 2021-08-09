e3e540852cff85ad560c66340f8d2880c806dd28
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-08-09 23:22:02 +0300

    file modfications

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from flask_script import Manager, Server
 # Creating app instance
 app = create_app('development')
 
-# manager = Manager(app)
+manager = Manager(app)
 manager.add_command('server', Server)
 
 
